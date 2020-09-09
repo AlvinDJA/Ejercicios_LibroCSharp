76298134f90c97bfe1b7cd11ba95a3bb0480a4ef
Alvin <alvindejesus88@gmail.com>
2020-09-08 20:32:29 -0400

    commit inicial

---
+++ Tarea 1/Ejercicio 2.4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ejercicio_2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double centigrados;
+            Console.WriteLine("Inserte el valor a convertir:");
+            centigrados = Convert.ToDouble(Console.ReadLine()); 
+            Console.WriteLine("El resultado en Fahrenheit es:{0}", centigrados*9/5+32);
+        }
+    }
+}

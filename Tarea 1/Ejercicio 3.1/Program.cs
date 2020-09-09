e7836ce1b9604953e2c51909d2e2f60cbea97bc3
Alvin <alvindejesus88@gmail.com>
2020-09-08 20:34:17 -0400

    commit inicial

---
+++ Tarea 1/Ejercicio 3.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ejercicio_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Inserte el numero");
+            num = Convert.ToInt32(Console.ReadLine());
+            if(num%2==0)
+                Console.WriteLine("Es par");
+            else
+                Console.WriteLine("Es Impar");
+        }
+    }
+}

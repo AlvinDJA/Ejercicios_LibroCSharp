934a67853bc5ca455616f93c6ec9e10d97665b56
Alvin <alvindejesus88@gmail.com>
2020-09-08 20:28:51 -0400

    commit inicial

---
+++ Tarea 1/Ejercicio 2.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ejercicio_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double lados;
+            double longuitud;
+            Console.WriteLine("Inserte la cantidad de lados:");
+            lados = Convert.ToDouble(Console.ReadLine()); 
+
+            Console.WriteLine("Inserte la longuitud:");
+            longuitud = Convert.ToDouble(Console.ReadLine()); 
+            
+            Console.WriteLine("El perimetro es {0}", longuitud*lados);
+           
+        }
+    }
+}

454e18fa77883e9e975e4004106a14b88cb96f99
Alvin <alvindejesus88@gmail.com>
2020-09-08 20:23:42 -0400

    commit inicial

---
+++ Tarea 1/Ejercicio 1.5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ejercicio1._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Alvin De Jesús Antigua");
+              Console.WriteLine("Im Dominican");
+               Console.WriteLine("I like programming");
+        }
+    }
+}
